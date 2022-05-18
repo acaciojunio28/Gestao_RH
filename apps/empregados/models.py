59bf2fd1214de949e8c616f346869b9719f821c2
unknown <acacio.junio28@gmail.com>
2022-05-18 10:43:55 -0300

    Criando o cálculo do banco de horas.

--- apps/empregados/models.py
+++ apps/empregados/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from apps.departamentos.models import departamento
 from apps.empresas.models import Empresa
+from django.db.models import Sum
 
 class Empregados(models.Model):
     nome=models.CharField(max_length=100, )
@@ -9,5 +10,13 @@ class Empregados(models.Model):
     departamento = models.ManyToManyField(departamento)
     empresa=models.ForeignKey(Empresa,on_delete=models.PROTECT,null=True,blank=True)
 
+    @property
+    def total_horas_extra(self):
+        return self.registrohoras_set.all().aggregate(Sum('horas'))['horas__sum']
+
+
     def __str__(self):
         return self.nome
+        return self.horas
+
+
